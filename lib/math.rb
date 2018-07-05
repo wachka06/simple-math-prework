abb305ccb8d74dd95c562d4a45e751f14b20451c
wachka06 <wachka06@gmail.com>
2018-07-05 21:50:28 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,12 +11,15 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num1 % num2
 end
 
 def square_root(num)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
